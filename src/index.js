3b353261320b318d362479868c0aa8bf82d90cbb
Andrew <N00153667@student.iadt.ie>
2019-01-22 16:00:10 +0000

    sss

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 <<<<<<< HEAD
+<<<<<<< HEAD
 import App from './components/App';
 import registerServiceWorker from './registerServiceWorker';
 
@@ -13,8 +14,18 @@ import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
+=======
+import App from './App';
+import * as serviceWorker from './serviceWorker';
+
+ReactDOM.render(<App />, document.getElementById('root'));
+
+>>>>>>> parent of b10d226... Recipe app
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: http://bit.ly/CRA-PWA
 serviceWorker.unregister();
+<<<<<<< HEAD
+>>>>>>> parent of b10d226... Recipe app
+=======
 >>>>>>> parent of b10d226... Recipe app
