2cb2566dcf82bc1d02b169d4d026df4433056670
Andrew <N00153667@student.iadt.ie>
2019-02-07 09:57:29 +0000

    Revert "Recipe app" This reverts commit b10d226b9f53fed84d975926590fd53f5e96de31.

--- src/index.js
+++ src/index.js
@@ -1,42 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
 import App from './components/App';
 import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
-=======
+
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-
-=======
-import App from './App';
-import * as serviceWorker from './serviceWorker';
-
-ReactDOM.render(<App />, document.getElementById('root'));
-
->>>>>>> parent of b10d226... Recipe app
-=======
-import App from './App';
-import * as serviceWorker from './serviceWorker';
-
-ReactDOM.render(<App />, document.getElementById('root'));
-
->>>>>>> parent of b10d226... Recipe app
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: http://bit.ly/CRA-PWA
 serviceWorker.unregister();
-<<<<<<< HEAD
-<<<<<<< HEAD
->>>>>>> parent of b10d226... Recipe app
-=======
->>>>>>> parent of b10d226... Recipe app
-=======
->>>>>>> parent of b10d226... Recipe app
